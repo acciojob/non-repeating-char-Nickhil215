0a22d9ce4b9e07282f2afc006149ac1dbc228487
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2023-04-13 16:15:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let str=prompt();
+let str=prompt("Enter a string");
 for(let i=0;i<str.length;i++){
     if(str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i))){
        console.log(str.charAt(i));
